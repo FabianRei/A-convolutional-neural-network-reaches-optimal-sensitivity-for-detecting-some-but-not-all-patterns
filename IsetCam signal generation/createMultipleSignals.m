4ac46742c5edbb143f05bcca56445ed9f5f8b1b1
Fabianrei <gFriessm20*>
2020-10-07 21:59:08 +0200

    refactoring

---
+++ IsetCam signal generation/createMultipleSignals.m
@@ -0,0 +1,12 @@
+function [img] = createMultipleSignals(parms,img)
+% Add created signals to multiple locations
+%   Detailed explanation goes here
+    newImg = ones(size(X));
+    signalPart = img-1;
+    for jj = 1:length(parms.signalLocation)
+        loc = parms.signalLocation(jj);
+        newImg = addSignalToLoc(loc, signalPart, newImg, parms);  
+    end
+    img = newImg;
+end
+
