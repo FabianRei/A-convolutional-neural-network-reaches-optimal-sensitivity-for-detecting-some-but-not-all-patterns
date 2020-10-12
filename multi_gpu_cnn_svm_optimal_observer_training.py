74d5d0b5d6ae76bb0e35d3867498c9960f7cc4ea
Fabianrei <gFriessm20*>
2020-10-12 18:51:40 +0200

    seeding experiment

--- multi_gpu_cnn_svm_optimal_observer_training.py
+++ multi_gpu_cnn_svm_optimal_observer_training.py
@@ -100,15 +100,11 @@ def run_across_contrast_levels(dirname, increase_network_depth=False, NetClass=N
 
 
 # Runs experiment. Each declared path contains .h5 signal arrays of varying contrast levels
-
 if __name__ == '__main__':
     # run automata experiments for various seeds
     # run a select group of experiments for various seeds.
     full_start = time.time()
-    super_path = '/share/wandell/data/reith/redo_experiments/sd_automata'
-    folder_paths = glob(os.path.join(super_path, 'seed*'))
-    sorted(folder_paths)
-    print(folder_paths)
+    folder_paths = ['/share/wandell/data/reith/redo_experiments/multiloc_addition/sd_seed_42']
     for folder_path in folder_paths:
         print(folder_path)
         fpaths = [p.path for p in os.scandir(folder_path) if p.is_dir()]
@@ -310,4 +306,22 @@ if __name__ == '__main__':
         for fpath in fpaths:
             run_across_contrast_levels(fpath, random_seed=seed)
     print(f"Whole program finished! It took {str(datetime.timedelta(seconds=time.time()-full_start))} hours:min:seconds")
+###############################################################
+
+if __name__ == '__main__':
+    # run automata experiments for various seeds
+    # run a select group of experiments for various seeds.
+    full_start = time.time()
+    super_path = '/share/wandell/data/reith/redo_experiments/sd_automata'
+    folder_paths = glob(os.path.join(super_path, 'seed*'))
+    sorted(folder_paths)
+    print(folder_paths)
+    for folder_path in folder_paths:
+        print(folder_path)
+        fpaths = [p.path for p in os.scandir(folder_path) if p.is_dir()]
+        seed = int(folder_path.split('_')[-1])
+        for fpath in fpaths:
+            run_across_contrast_levels(fpath, random_seed=seed)
+    print(f"Whole program finished! It took {str(datetime.timedelta(seconds=time.time()-full_start))} hours:min:seconds")
+
 """
