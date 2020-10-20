ace83e1da39dd7afc4b8cabcecf07cfda35abd16
Brian Wandell <wandell@stanford.edu>
2020-10-20 10:34:15 -0700

    Thinking with FR about edits to imageHarmonic and imageReplication

--- IsetCam_signal_generation/createMultipleSignals.m
+++ IsetCam_signal_generation/createMultipleSignals.m
@@ -1,12 +1,27 @@
-function [img] = createMultipleSignals(parms,img, X)
+function [newImg] = createMultipleSignals(img,parms)
 % Add created signals to multiple locations
-%   Detailed explanation goes here
-    newImg = ones(size(X));
-    signalPart = img-1;
-    for jj = 1:length(parms.signalLocation)
-        loc = parms.signalLocation(jj);
-        newImg = addSignalToLoc(loc, signalPart, newImg, parms);  
-    end
-    img = newImg;
+%
+% Synopsis
+%   [newImg] = createMultipleSignals(img,parms)
+%
+% Brief description
+%  Replicates a harmonic image (img) at multiple locations to create a new
+%  image.
+%
+%  Expected: img is a harmonic with a mean of 1.
+%  Output img has the harmonic replicated at signalLocal positions.
+%
+%  parms must have .signalLocation, .gridSize, .row, .col
+%
+% See also
+%   imageHarmonic
+
+newImg = ones(size(img));  % 
+signalPart = img-1;   % img is a harmonic with mean 1.  We get the contrast.
+for jj = 1:length(parms.signalLocation)
+    loc = parms.signalLocation(jj);
+    newImg = addSignalToLoc(loc, signalPart, newImg, parms);
+end
+
 end
 
